d83da3f17e3d6c28fbef69dec555235d01d74113
TubiOb <Tubiobaloluwa@gmail.com>
2024-01-25 18:06:48 +0100

    Structured loginForm

--- src/components/LoginForm.jsx
+++ src/components/LoginForm.jsx
@@ -1,8 +1,21 @@
-import React from 'react'
-import { Box, Image, Text, FormControl, Hide } from '@chakra-ui/react';
+import React, { useState } from 'react'
+import { Box, Image, Text, Hide } from '@chakra-ui/react';
 import Students from '../assets/Students-rafiki.svg'
+import CustomInput from './CustomInput';
+import { NavLink } from 'react-router-dom';
 
 const LoginForm = () => {
+    const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+
+  const handleEmailChange = (value) => {
+    setEmail(value);
+  };
+
+  const handlePasswordChange = (value) => {
+    setPassword(value);
+  };
+
   return (
     <Box display='flex' flexDir='row' alignItems='center' justifyContent='space-between' w='full' h='100svh'>
         <Hide below='lg'>
@@ -20,9 +33,18 @@ const LoginForm = () => {
                     <Text as='p' fontSize={['sm', 'base', 'lg']}>Enter your details to get signed in to your account</Text>
                 </Box>
 
-                <FormControl w={['95%', '80%']} mt='2' display='flex' flexDir='column' justifyContent='space-between' gap='6'>
-            
-                </FormControl>
+                <form className='w-[95%] md:w-[80%] mt-2 flex flex-col justify-between gap-6'>
+                    <CustomInput type="email" label="Email" placeholder="Email" value={email} onChange={handleEmailChange} />
+                    <CustomInput type="password" label="Password" placeholder="Password" value={password} onChange={handlePasswordChange} />
+
+                    <div className='flex flex-wrap w-full -mt-5 px-3 items-center justify-between'>
+                        <p className='text-xs md:text-sm text-blue-600 hover:underline cursor-pointer'><NavLink to='/forgot-password' >Forgot password?</NavLink></p>
+                    </div>
+
+                    <button type="submit" className='text-blue-400 px-2 py-2 rounded-xl w-[70%] mx-auto bg-white font-semibold shadow-neutral-200 border-neutral-50 shadow-md transition duration-300  hover:font-semibold hover:bg-blue-400 hover:text-white hover:shadow-neutral-300 text-sm md:text-lg flex items-center justify-center' >Sign In</button>
+                </form>
+
+                <p className='text-blue-600 my-3 text-xs md:text-sm '>Don't have an account? <NavLink to='/signup' className='underline cursor-pointer'>Sign up</NavLink></p>
             </Box>
         </Box>
     </Box>
--- src/pages/LandingPage.js
+++ src/pages/LandingPage.js
@@ -10,7 +10,7 @@ const LandingPage = () => {
     <Box display='flex' alignItems='center' justifyContent='center' w='full' h={['screen', '100svh']} flexDir={['column', 'row']} bg='mintcream' >
         <Box display='flex' alignItems='center' justifyContent='space-between' w={['95%', '80%']} mx='auto' h='full' p='3' flexDir={['column', 'row']} gap='10' rounded='lg' >
             <NavLink to='/login' w={['90%', '80']} h={['300px', '300px']}>
-                <Box display='flex' flexDir='column' alignItems='center' justifyContent='center' boxShadow='base' bg='white' mx='auto' _hover={{ boxShadow: 'xl'}} py='3' cursor='pointer' gap='3' textAlign='center' rounded='md' >
+                <Box display='flex' flexDir='column' alignItems='center' justifyContent='center' boxShadow='base' bg='white' mx='auto' _hover={{ boxShadow: 'xl'}} py='3' cursor='pointer' gap='3' textAlign='center' rounded='xl' >
                     <Image src={Student} alt='Students Login' objectFit='cover' w='full' h={['80%', '250px']} />
                     <Text as='h4' fontSize={['smaller', 'medium', 'large']} fontWeight='semibold' px='2'>Student</Text>
                     <Text as='p' fontSize={['xs', 'sm', 'md']} fontWeight='light'>Manage user roles, permissions, and system settings to ensure the smooth operation of the school website.</Text>
@@ -19,7 +19,7 @@ const LandingPage = () => {
             
 
             <NavLink to='/login' w={['90%', '80']} h={['300px', '300px']}>
-                <Box display='flex' flexDir='column' alignItems='center' justifyContent='center' boxShadow='base' bg='white' mx='auto' _hover={{ boxShadow: 'xl'}} py='3' cursor='pointer' gap='3' textAlign='center' rounded='md'>
+                <Box display='flex' flexDir='column' alignItems='center' justifyContent='center' boxShadow='base' bg='white' mx='auto' _hover={{ boxShadow: 'xl'}} py='3' cursor='pointer' gap='3' textAlign='center' rounded='xl'>
                     <Image src={Admin} alt='Admin Login' objectFit='cover' w='full' h={['80%', '250px']} />
                     <Text as='h4' fontSize={['smaller', 'medium', 'large']} fontWeight='semibold' px='2'>Admin</Text>
                     <Text as='p' fontSize={['xs', 'sm', 'md']} fontWeight='light'>Create, manage, and grade assignments to facilitate effective teaching and assessment for students.</Text>
@@ -28,7 +28,7 @@ const LandingPage = () => {
             
 
             <NavLink to='/login' w={['90%', '80']} h={['300px', '300px']}>
-                <Box display='flex' flexDir='column' alignItems='center' justifyContent='center' boxShadow='base' bg='white' mx='auto' _hover={{ boxShadow: 'xl'}} py='3' cursor='pointer' gap='3' textAlign='center' rounded='md'>
+                <Box display='flex' flexDir='column' alignItems='center' justifyContent='center' boxShadow='base' bg='white' mx='auto' _hover={{ boxShadow: 'xl'}} py='3' cursor='pointer' gap='3' textAlign='center' rounded='xl'>
                     <Image src={Teacher} alt='Teahcers Login' objectFit='cover' w='full' h={['80%', '250px']} />
                     <Text as='h4' fontSize={['smaller', 'medium', 'large']} fontWeight='semibold' px='2'>Teacher</Text>
                     <Text as='p' fontSize={['xs', 'sm', 'md']} fontWeight='light'>Access course materials, submit assignments, and view grades for a personalized learning experience.</Text>
